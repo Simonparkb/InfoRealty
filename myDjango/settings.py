2b8c2c8e031b4f659f614e067742917fd1d6e8a3
SIMON\parkk <simonparkb@gmail.com>
2024-09-28 21:34:20 +0900

    .

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -71,7 +71,11 @@ TEMPLATES = [
     },
 ]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 WSGI_APPLICATION = 'myDjango.wsgi.application'
 
 
