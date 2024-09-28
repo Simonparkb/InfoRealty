d9888b2575238797ebd5f67bfde70434fcfc427c
SIMON\parkk <simonparkb@gmail.com>
2024-09-28 19:45:24 +0900

    .

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -4,6 +4,6 @@ from .views import kakaomap, index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('infoReality/', kakaomap, name='kakaomap'),
+    path('infoRealty/', kakaomap, name='kakaomap'),
     path('find_shortest_route/', views.find_shortest_route, name='find_shortest_route')
 ]
