1ee27c12394deab2fbfb295b5b8a751059e28756
SIMON\parkk <simonparkb@gmail.com>
2024-09-28 19:29:10 +0900

    .

--- myDjango/asgi.py
+++ myDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for myDjango project.
+ASGI auth for myDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- myDjango/wsgi.py
+++ myDjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myDjango project.
+WSGI auth for myDjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
