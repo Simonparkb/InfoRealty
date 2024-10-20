4a5d9c87552294c190a78eab3812ff87d9a6e4d6
SIMON\parkk <simonparkb@gmail.com>
2024-10-20 13:52:00 +0900

    .

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     # # "43.203.248.192",
-    "ec2-43-203-248-192.ap-northeast-2.compute.amazonaws.com"
+    # "ec2-43-203-248-192.ap-northeast-2.compute.amazonaws.com"
 ]
 
 
