0b4ffd58a792162725eb4172ccd3ff52a780e17f
SIMON\parkk <simonparkb@gmail.com>
2024-10-27 21:58:57 +0900

    .

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -26,9 +26,10 @@ SECRET_KEY = 'django-insecure-+0*ejxg5=0cj*wq-nuxm&#lj=6ic3iew_*ktyu%hd_^-gkt#$r
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "43.202.40.102",
+    # "43.202.40.102",
     "http://ec2-43-202-40-102.ap-northeast-2.compute.amazonaws.com/",
-    "ec2-43-202-40-102.ap-northeast-2.compute.amazonaws.com",
+    # "ec2-43-202-40-102.ap-northeast-2.compute.amazonaws.com",
+
 ]
 
 
