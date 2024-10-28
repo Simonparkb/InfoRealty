1f1bb27653a428bfb8844fb74a8fc007620627c1
SIMON\parkk <simonparkb@gmail.com>
2024-10-28 23:47:20 +0900

    .

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -3,9 +3,9 @@ from . import views
 from .views import kakaomap, departures, arrivals, services,station_map,station_detail,add_station,delete_station,get_line_images
 
 urlpatterns = [
-    path('', departures, name='departures'),
-    path('arrivals/', arrivals, name='arrivals'),
-    path('services/', services, name='services'),
+    path('A', departures, name='departures'),
+    path('B/', arrivals, name='arrivals'),
+    path('C/', services, name='services'),
     path('infoRealty/', kakaomap, name='kakaomap'),
     path('find_shortest_route/', views.find_shortest_route, name='find_shortest_route'),
     path('find_nearest_stations/', views.find_nearest_stations, name='find_nearest_stations'),
